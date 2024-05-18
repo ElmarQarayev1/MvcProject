0e5ebf36e69b498f9bc6abc50e56c852381e67f8
ElmarQarayev1 <elmareg@code.edu.az>
2024-05-18 12:56:42 +0400

    dashboardun viewsi hazirlandi

---
+++ MvcProject/MvcProject/Areas/Manage/Controllers/DashboardController.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcProject.Areas.Manage.Controllers
+{
+	[Area("manage")]
+	public class DashboardController:Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+		
+	}
+}
+
