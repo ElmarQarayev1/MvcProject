12b77801868197a3fbfe4cee80bba1e6bcb720ee
ElmarQarayev1 <elmareg@code.edu.az>
2024-05-21 12:36:57 +0400

    about hissesi yazildi

---
+++ MvcProject/MvcProject/Controllers/AboutController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using MvcProject.Data;
+using MvcProject.ViewModels;
+
+namespace MvcProject.Controllers
+{
+	public class AboutController:Controller
+	{
+        private readonly AppDbContext _context;
+
+        public AboutController(AppDbContext context)
+		{
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            AboutViewModel aboutViewModel = new AboutViewModel()
+            {
+                Infos = _context.Infos.ToList(),
+                Teachers = _context.Teachers.Take(4).ToList()
+
+            };
+             return View(aboutViewModel);
+        }
+	}
+}
+
---
+++ MvcProject/MvcProject/ViewModels/AboutViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using MvcProject.Models;
+
+namespace MvcProject.ViewModels
+{
+	public class AboutViewModel
+	{
+		public List<Info> Infos { get; set; }
+		public List<Teacher> Teachers { get; set; }
+	}
+}
